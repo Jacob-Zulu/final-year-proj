a792e8d5a4ec89545490c82b24b96e6589352dd0
Jacob Zulu <jacob.zulu@outlook.com>
2022-07-11 10:07:01 +0200

    First commit

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1om8e@4qtcdw-^b$(##uh@eekzm36+aro2$=&5=ttrojtye8-7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'door-system.azurewebsites.net']
 
 
 # Application definition
